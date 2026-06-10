14ab4923b14360874b67dd695ffd25a1066ce69a
payalpatil1730 <payalpatil0830@gmail.com>
2026-06-10 15:10:02 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,18 +1,4 @@
-# SPDX-FileCopyrightText: 2015 Eric Larson
-#
-# SPDX-License-Identifier: Apache-2.0
+from .core import contents, where
 
-"""CacheControl import Interface.
-
-Make it easy to import from cachecontrol without long namespaces.
-"""
-__author__ = "Eric Larson"
-__email__ = "eric@ionrock.org"
-__version__ = "0.12.11"
-
-from .wrapper import CacheControl
-from .adapter import CacheControlAdapter
-from .controller import CacheController
-
-import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+__all__ = ["contents", "where"]
+__version__ = "2022.12.07"
--- __main__.py
+++ __main__.py
@@ -1,3 +1,12 @@
-from .cli import main
+import argparse
 
-main()
+from pip._vendor.certifi import contents, where
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--contents", action="store_true")
+args = parser.parse_args()
+
+if args.contents:
+    print(contents())
+else:
+    print(where())
