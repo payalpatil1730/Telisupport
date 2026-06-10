97ffe20cbbc3f320658d77964ceea0647a008319
payalpatil1730 <payalpatil0830@gmail.com>
2026-06-10 15:04:48 +0530

    Add files via upload

--- encoding.py
+++ encoding.py
@@ -1,54 +1,36 @@
-from __future__ import annotations
+import codecs
+import locale
+import re
+import sys
+from typing import List, Tuple
 
-import base64
-import string
-import struct
-import typing as t
+BOMS: List[Tuple[bytes, str]] = [
+    (codecs.BOM_UTF8, "utf-8"),
+    (codecs.BOM_UTF16, "utf-16"),
+    (codecs.BOM_UTF16_BE, "utf-16-be"),
+    (codecs.BOM_UTF16_LE, "utf-16-le"),
+    (codecs.BOM_UTF32, "utf-32"),
+    (codecs.BOM_UTF32_BE, "utf-32-be"),
+    (codecs.BOM_UTF32_LE, "utf-32-le"),
+]
 
-from .exc import BadData
-
-
-def want_bytes(
-    s: str | bytes, encoding: str = "utf-8", errors: str = "strict"
-) -> bytes:
-    if isinstance(s, str):
-        s = s.encode(encoding, errors)
-
-    return s
-
-
-def base64_encode(string: str | bytes) -> bytes:
-    """Base64 encode a string of bytes or text. The resulting bytes are
-    safe to use in URLs.
-    """
-    string = want_bytes(string)
-    return base64.urlsafe_b64encode(string).rstrip(b"=")
+ENCODING_RE = re.compile(rb"coding[:=]\s*([-\w.]+)")
 
 
-def base64_decode(string: str | bytes) -> bytes:
-    """Base64 decode a URL-safe string of bytes or text. The result is
-    bytes.
-    """
-    string = want_bytes(string, encoding="ascii", errors="ignore")
-    string += b"=" * (-len(string) % 4)
+def auto_decode(data: bytes) -> str:
+    """Check a bytes string for a BOM to correctly detect the encoding
 
-    try:
-        return base64.urlsafe_b64decode(string)
-    except (TypeError, ValueError) as e:
-        raise BadData("Invalid base64-encoded data") from e
-
-
-# The alphabet used by base64.urlsafe_*
-_base64_alphabet = f"{string.ascii_letters}{string.digits}-_=".encode("ascii")
-
-_int64_struct = struct.Struct(">Q")
-_int_to_bytes = _int64_struct.pack
-_bytes_to_int = t.cast("t.Callable[[bytes], tuple[int]]", _int64_struct.unpack)
-
-
-def int_to_bytes(num: int) -> bytes:
-    return _int_to_bytes(num).lstrip(b"\x00")
-
-
-def bytes_to_int(bytestr: bytes) -> int:
-    return _bytes_to_int(bytestr.rjust(8, b"\x00"))[0]
+    Fallback to locale.getpreferredencoding(False) like open() on Python3"""
+    for bom, encoding in BOMS:
+        if data.startswith(bom):
+            return data[len(bom) :].decode(encoding)
+    # Lets check the first two lines as in PEP263
+    for line in data.split(b"\n")[:2]:
+        if line[0:1] == b"#" and ENCODING_RE.search(line):
+            result = ENCODING_RE.search(line)
+            assert result is not None
+            encoding = result.groups()[0].decode("ascii")
+            return data.decode(encoding)
+    return data.decode(
+        locale.getpreferredencoding(False) or sys.getdefaultencoding(),
+    )
